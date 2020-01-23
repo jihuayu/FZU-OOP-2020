2e08be61aaf4a210faeee762f92833e0d4488ab3
jihuayu <8042833@qq.com>
2020-01-23 13:35:57 +0800

    添加vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,41 @@
+module.exports = {
+    title: 'FZU OOP 2020',
+    description: '福州大学 2020 年面向对象程序设计作业集散仓库',
+    serviceWorker: false,
+    themeConfig: {
+        repo: 'jihuayu/FZU-OOP-2020',
+        editLinks: true,
+        docsDir: 'docs',
+        locales: {
+            '/': {
+                nav: [
+                    {
+                        text: '班级博客',
+                        link: 'https://edu.cnblogs.com/campus/fzu/2020OOP'
+                    }
+                ],
+                sidebar: [
+                    '/tips',
+                    {
+                        title: '作业',
+                        collapsable: true,
+                        children: [
+                            '/works/work0',
+                            '/works/work1'
+                        ]
+                    },
+                    {
+                        title: '思考题',
+                        collapsable: true,
+                        children: [
+                            '/thinks/think0121',
+                            '/thinks/think0122',
+
+                        ]
+                    }
+                ]
+            }
+            
+        }
+    }
+}
---
+++ docs/works/work0.md
@@ -0,0 +1,7 @@
+# 第零次作业
+1. 开设Github，注册账号：https://github.com/，请选择一个合适的用户名。
+2. 建立个人技术博客，在 https://www.cnblogs.com/ 上注册开设博客。
+3. 关注以下博客: [老师](https://home.cnblogs.com/u/easteast/)，[助教](https://home.cnblogs.com/u/jhy16193335/)
+4. 加入班级：链接见QQ群。
+5. 把博客园的编辑器切换为markdown。博客请使用Markdown格式（在博客园“管理”->“选项”中，将默认编辑器改为Markdown），[Markdown入门参考](http://www.cnblogs.com/math/p/se-tools-001.html)
+6. 发一篇博客介绍你自己，让我能够印象深刻的记住你。也许是你的才艺、也许是你的经历、也许是你曾经取得的成绩或荣誉、也许是你曾经经历过的某件事或交往过的某个朋友，或你曾有的某个记忆，甚至是你的颜值。介绍一下你的不平凡，让我真正记住你。
